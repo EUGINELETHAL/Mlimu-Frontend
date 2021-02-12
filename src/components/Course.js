dc69cf114b0f014b2dfab7aa21f4e6073aad3e49
EUGINELETHAL <ochungeugine@gmail.com>
2021-02-12 17:04:27 +0300

    added mdb

--- src/components/Course.js
+++ src/components/Course.js
@@ -1,15 +1,64 @@
 import React from 'react'
+import { MDBBtn, MDBCol, MDBContainer, MDBRow, MDBAnimation} from "mdbreact";
+import {
+  MDBEdgeHeader,
+  MDBFreeBird,
+  MDBCardBody,
+  MDBIcon,
+  MDBCard,
+  MDBCardTitle,
+  MDBCardImage,
+  MDBCardText,
+  MDBNavLink
+} from 'mdbreact';
+import './HomePage.css';
+import "../index.css";
+
 
 const Course = ({course}) => {
   return (
-    <div>
-    
-        
-        <div className="title">{course.name}</div>
-        <div className="author">{course.author}</div>
-      
   
-    </div>
+         
+                  <MDBCol md='4'>
+                    <MDBAnimation reveal type='fadeInLeft'>
+                      <MDBCard cascade className='my-3 grey lighten-4'>
+                        <MDBCardImage
+                          cascade
+                          className='img-fluid'
+                          src='https://mdbootstrap.com/wp-content/uploads/2016/08/mdb.jpg'
+                        />
+                        <MDBCardBody cascade className='text-center'>
+                          <MDBCardTitle>
+                            <MDBIcon
+                              icon='css3'
+                              brand
+                              className='pink-text pr-2'
+                            />
+                            <strong>{course.author}</strong>
+                          </MDBCardTitle>
+                          <MDBCardText>
+                          {course.name}
+                          </MDBCardText>
+                          <MDBNavLink
+                            tag='button'
+                            to='/css'
+                            color='mdb-color'
+                            className='btn btn-outline-mdb-color btn-sm btn-rounded d-inline'
+                           
+                          >
+                           Enroll to Course
+                          </MDBNavLink>
+                        </MDBCardBody>
+                      </MDBCard>
+                    </MDBAnimation>
+                  </MDBCol>  
+                
+
+  
+        
+       
+  
+  
   )
 }
 
--- src/components/Courselist.js
+++ src/components/Courselist.js
@@ -1,7 +1,21 @@
 import React, { useContext } from "react";
 import {Coursecontext}  from '../contexts.js/coursecontext';
 import Course from "./Course";
-
+import { MDBBtn, MDBCol, MDBContainer, MDBRow } from "mdbreact";
+import {
+  MDBEdgeHeader,
+  MDBFreeBird,
+  MDBCardBody,
+  MDBIcon,
+  MDBCard,
+  MDBCardTitle,
+  MDBCardImage,
+  MDBCardText,
+  MDBAnimation,
+  MDBNavLink
+} from 'mdbreact';
+import './HomePage.css';
+import "../index.css";
 
 function Courselist() {
   const {Courses} = useContext(Coursecontext);
@@ -9,14 +23,35 @@ function Courselist() {
   
 
     return Courses.length ? (
-      <div className="book-list">
-        <ul>
-         
-    {courselist1}
-  
-        </ul>
+   
        
-      </div>
+        <MDBRow>
+              <MDBCol md='12' className='mt-4'>
+                <h2 className='text-center my-5 font-weight-bold'>
+                  Why is it so great?
+                </h2>
+                <p className='text-center text-muted mb-1'>
+                  Google has designed a Material Design to make the web more
+                  beautiful and more user-friendly.
+                </p>
+                <p className='text-center text-muted mb-1'>
+                  Twitter has created a Bootstrap to support you in faster and
+                  easier development of responsive and effective websites.
+                </p>
+                <p className='text-center text-muted'>
+                  We present you a framework containing the best features of
+                  both of them - Material Design for Bootstrap.
+                </p>
+                <hr className='my-5' />
+
+                </MDBCol>  
+                {courselist1}
+                  </MDBRow>  
+  
+              
+       
+       
+     
     ) : (
       <div className="empty">No books to read. Hello free time :).</div>
     );
